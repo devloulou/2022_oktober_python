3e335ff5cae22205d08bb043d7573a2e9f427a08
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-10-13 20:15:24 +0200

    1. alkalom

---
+++ first_python.py
@@ -0,0 +1,37 @@
+# változók
+# szám típusú
+
+# 2 + 4 = 6
+
+# adattípusa neve = értéke
+# int szam = 2;
+
+# a változó: neve, értéke, az változó adattípusa bármikor megváltoztatható
+# bármikor bármilyen értéket adhatok a változónak
+
+# értékadás: a memóriában létrejön egy, a változó nevével jelzett memória címzés
+# és arra a memória területre eltárolódik a változó értéke és típusa
+
+# integer - int
+# a pythonban minden egy objektum
+szam = 2
+
+# string - szöveges adat
+szam2 = 4
+print(szam2)
+
+szam2 = "aSDhgSdhS3523523@/sdsDhgDDhSDhsdy"
+print(szam2)
+# a python dinamikusosan tipusos
+# dynamic type
+
+# float - lebegőpontos szám
+szam3 =  5.1
+
+complex_number = 2+3j
+print(complex_number)
+
+#######################################################################
+# stringekkel foglalkozing, string műveletekkel
+# slicing
+# összetett adattípusokba: list, tuple, dictionary
